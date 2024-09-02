299bb9ac03cdda7edce7e1cd1a727a791c80ce1a
k1105 <kanapon1105@yahoo.co.jp>
2024-09-03 01:00:45 +0900

    型の宣言をtypes/global.d.tsに移行

--- components/Sketch.tsx
+++ components/Sketch.tsx
@@ -4,37 +4,6 @@ import { NextReactP5Wrapper } from "@p5-wrapper/next";
 import { updateRelation } from "@/lib/updateRelation";
 import { updatePeople } from "@/lib/updatePeople";
 
-interface Bbox {
-  confidence: number;
-  bbox: [number, number, number, number];
-}
-
-interface PersonAttribute {
-  id: number;
-  speed: number;
-  bbox: Bbox;
-  center: () => { x: number; y: number };
-}
-
-class Person implements PersonAttribute {
-  id: number;
-  speed: number;
-  bbox: Bbox;
-
-  constructor(id: number, speed: number, bbox: Bbox) {
-    this.id = id;
-    this.speed = speed;
-    this.bbox = bbox;
-  }
-
-  center() {
-    return {
-      x: (this.bbox.bbox[0] + this.bbox.bbox[2]) / 2,
-      y: (this.bbox.bbox[1] + this.bbox.bbox[3]) / 2,
-    };
-  }
-}
-
 function sketch(p5: P5CanvasInstance) {
   let bboxes: Bbox[] = [];
   let prevBboxes: Bbox[] = [];
--- lib/updateRelation.ts
+++ lib/updateRelation.ts
@@ -1,34 +1,3 @@
-interface Bbox {
-  confidence: number;
-  bbox: [number, number, number, number];
-}
-
-interface PersonAttribute {
-  id: number;
-  speed: number;
-  bbox: Bbox;
-  center: () => { x: number; y: number };
-}
-
-class Person implements PersonAttribute {
-  id: number;
-  speed: number;
-  bbox: Bbox;
-
-  constructor(id: number, speed: number, bbox: Bbox) {
-    this.id = id;
-    this.speed = speed;
-    this.bbox = bbox;
-  }
-
-  center() {
-    return {
-      x: (this.bbox.bbox[0] + this.bbox.bbox[2]) / 2,
-      y: (this.bbox.bbox[1] + this.bbox.bbox[3]) / 2,
-    };
-  }
-}
-
 type Props = {
   people: Person[];
   bboxes: Bbox[];
