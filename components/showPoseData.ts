7a703a70d357871dd191efa6ba3330f01e230919
k1105 <kanapon1105@yahoo.co.jp>
2025-03-20 11:47:16 +0900

    軸ラベルの表示位置を修正

--- components/showPoseData.ts
+++ components/showPoseData.ts
@@ -19,12 +19,12 @@ const showPoseData = ({
   }
   p5.stroke(255);
   const p1 = {
-    x: pose.keypoints[5].x + (pose.keypoints[5].x + pose.keypoints[6].x) / 2,
-    y: pose.keypoints[5].y + (pose.keypoints[5].y + pose.keypoints[6].y) / 2,
+    x: (pose.keypoints[5].x + pose.keypoints[6].x) / 2,
+    y: (pose.keypoints[5].y + pose.keypoints[6].y) / 2,
   };
   const p2 = {
-    x: pose.keypoints[11].x + (pose.keypoints[11].x + pose.keypoints[12].x) / 2,
-    y: pose.keypoints[11].y + (pose.keypoints[12].y + pose.keypoints[12].y) / 2,
+    x: (pose.keypoints[11].x + pose.keypoints[12].x) / 2,
+    y: (pose.keypoints[12].y + pose.keypoints[12].y) / 2,
   };
   if (p1.x !== 0 && p2.x !== 0 && p1.y !== 0 && p2.y !== 0) {
     p5.line(p1.x * scale, p1.y * scale, p2.x * scale, p2.y * scale);
