8cac134c4efc4db3aecbd973904949d91b2d59b6
k1105 <kanapon1105@yahoo.co.jp>
2025-03-21 22:04:28 +0900

    テキストの表示サイズを修正

--- components/showCharacter.ts
+++ components/showCharacter.ts
@@ -36,14 +36,14 @@ const showCharacter = ({
           p5.push();
           const lastBox = boxHistory[index * step].bbox;
           const h = (lastBox[3] - lastBox[1]) * 1.2;
-          p5.translate(charData.x * h, charData.y * h);
-          p5.fill(255, 255 * (6 - index));
+
+          p5.fill(255, 255, 255, 255 * 0.8 ** index);
           p5.translate(
-            (lastBox[0] + lastBox[2]) / 2,
-            (lastBox[1] + lastBox[3]) / 2
+            charData.x * h + (lastBox[0] + lastBox[2]) / 2,
+            charData.y * h + (lastBox[1] + lastBox[3]) / 2
           );
           if (rotationActive && person.pose && index == 0) p5.rotate(rot);
-          p5.textSize(h - index * 150);
+          p5.textSize(h * 0.8 ** index);
           p5.text(
             charData.char,
             (lastBox[0] - lastBox[2]) / 2,
@@ -54,40 +54,6 @@ const showCharacter = ({
         }
       }
     });
-
-    // else {
-    //   const box = person.smoothedBbox.bbox;
-    //   const h = (box[3] - box[1]) * 1.2;
-    //   p5.push();
-    //   p5.translate(
-    //     person.displayCharacter.x * h,
-    //     person.displayCharacter.y * h
-    //   );
-
-    //   p5.translate((box[0] + box[2]) / 2, (box[1] + box[3]) / 2);
-    //   const rot =
-    //     person.bodyAxis.p1.x *
-    //       person.bodyAxis.p1.y *
-    //       person.bodyAxis.p2.x *
-    //       person.bodyAxis.p2.y !==
-    //     0
-    //       ? Math.atan2(
-    //           person.bodyAxis.p1.y - person.bodyAxis.p2.y,
-    //           person.bodyAxis.p1.x - person.bodyAxis.p2.x
-    //         ) +
-    //         Math.PI / 2
-    //       : 0;
-    //   // console.log(rot);
-    //   if (rotationActive && person.pose) p5.rotate(rot);
-    //   p5.textSize(h);
-    //   p5.text(
-    //     person.displayCharacter.char,
-    //     (box[0] - box[2]) / 2,
-    //     h * 0.8 + (box[1] - box[3]) / 2,
-    //     box[2] - box[0]
-    //   );
-    //   p5.pop();
-    // }
   }
 };
 
